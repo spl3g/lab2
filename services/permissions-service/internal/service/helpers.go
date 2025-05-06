c47c89b8f88e6a7e31e8157d962c2a535da0a839
spl3g <spleefer6@yandex.ru>
2025-05-06 18:07:48 +0300

    Add more helpers

--- services/permissions-service/internal/service/helpers.go
+++ services/permissions-service/internal/service/helpers.go
@@ -1,12 +1,43 @@
 package service
 
-import "s0709-22/internal/proxyproto"
+import (
+	"github.com/spl3g/lab2/internal/proxyproto"
+	"github.com/spl3g/lab2/internal/userdb"
+)
 
-func RespondError(code uint32, msg string) (*proxyproto.ConnectResponse, error) {
+func ConnectRespondError(code uint32, msg string) (*proxyproto.ConnectResponse, error) {
 	return &proxyproto.ConnectResponse{
 		Error: &proxyproto.Error{
 			Code:    code,
 			Message: msg,
 		},
 	}, nil
+}
+
+func SubscribeRespondError(code uint32, msg string) (*proxyproto.SubscribeResponse, error) {
+	return &proxyproto.SubscribeResponse{
+		Error: &proxyproto.Error{
+			Code:    code,
+			Message: msg,
+		},
+	}, nil
+}
+
+func PublishRespondError(code uint32, msg string) (*proxyproto.PublishResponse, error) {
+	return &proxyproto.PublishResponse{
+		Error: &proxyproto.Error{
+			Code:    code,
+			Message: msg,
+		},
+	}, nil
+}
+
+func UserToCreateUserParams(user userdb.User) userdb.CreateUserParams {
+	return userdb.CreateUserParams{
+		ID:         user.ID,
+		Username:   user.Username,
+		GivenName:  user.GivenName,
+		FamilyName: user.FamilyName,
+		Enabled:    user.Enabled,
+	}
 }
